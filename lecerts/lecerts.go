da3a2262ef9d4f07625894564d51295e69a50011
Krasnov Sergey <sergey.krasnov@edgecenter.ru>
2026-07-10 15:59:23 +0300

    feat(TF-115): support cert_type in lecert issue flow

--- lecerts/lecerts.go
+++ lecerts/lecerts.go
@@ -2,6 +2,7 @@ package lecerts
 
 import (
 	"context"
+	"encoding/json"
 )
 
 type LECertService interface {
@@ -12,6 +13,21 @@ type LECertService interface {
 	CancelLECert(ctx context.Context, resourceID int64, active bool) error
 }
 
+type LECertIssueService interface {
+	IssueLECert(ctx context.Context, resourceID int64, req *IssueRequest) error
+}
+
+type CertType string
+
+const (
+	CertTypeLE   CertType = "LE"
+	CertTypeMDDC CertType = "MDDC"
+)
+
+type IssueRequest struct {
+	CertType CertType `json:"cert_type,omitempty"`
+}
+
 type LECertStatus struct {
 	ID       int              `json:"ssl_id"`
 	Statuses []LEStatusDetail `json:"statuses"`
@@ -19,6 +35,7 @@ type LECertStatus struct {
 	Finished *string          `json:"finished"`
 	Active   bool             `json:"active"`
 	Resource int              `json:"resource"`
+	CertType CertType         `json:"cert_type,omitempty"`
 }
 
 type LEStatusDetail struct {
@@ -26,4 +43,24 @@ type LEStatusDetail struct {
 	Error   string `json:"error"`
 	Details string `json:"details"`
 	Created string `json:"created"`
+}
+
+func (s *LECertStatus) UnmarshalJSON(data []byte) error {
+	type alias LECertStatus
+
+	aux := struct {
+		alias
+		CertTypeLegacy CertType `json:"cet_type"`
+	}{}
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	*s = LECertStatus(aux.alias)
+	if s.CertType == "" {
+		s.CertType = aux.CertTypeLegacy
+	}
+
+	return nil
 }
